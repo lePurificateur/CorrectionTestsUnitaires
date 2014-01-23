0d3972f358d6e051760f5892e0c7f6fac1c2cfb7
klaus <klaus@joffrey>
2014-01-23 18:56:41 +0100

    * InterfacePoint getPoint() added in the InterfacePoint test case.

--- TPTestsUnitaires/src/tests/TestPoint.java
+++ TPTestsUnitaires/src/tests/TestPoint.java
@@ -10,17 +10,32 @@ import sources.ImplementPoint;
 public class TestPoint
 {
 
+	// Pour pouvoir connecter cette classe de tests à n'importe quelle implémentation.
+	// Il vous suffit de remplacer l'instanciation ici par celle de votre classe point.
+	private InterfacePoint getPoint()
+	{
+		return new ImplementPoint();
+	}
+
+	private InterfacePoint getPoint(int abs, int ord)
+	{
+		InterfacePoint p = getPoint();
+		p.setAbs(abs);
+		p.setOrd(ord);
+		return p;
+	}
+	
 	@Test
 	public void testPointIntInt()
 	{
-		InterfacePoint p = new ImplementPoint(3, 2);
+		InterfacePoint p = getPoint(3, 2);
 		assertTrue(p.getAbs() == 3 && p.getOrd() == 2);
 	}
 
 	@Test
 	public void testGetOrd()
 	{
-		InterfacePoint p = new ImplementPoint();
+		InterfacePoint p = getPoint();
 		p.setOrd(5);
 		assertTrue(p.getOrd() == 5);
 		p.setOrd(-1);
@@ -32,7 +47,7 @@ public class TestPoint
 	@Test
 	public void testGetAbs()
 	{
-		InterfacePoint p = new ImplementPoint();
+		InterfacePoint p = getPoint();
 		p.setAbs(5);
 		assertTrue(p.getAbs() == 5);
 		p.setAbs(-1);
@@ -44,22 +59,22 @@ public class TestPoint
 	@Test
 	public void testEquals()
 	{
-		InterfacePoint p = new ImplementPoint(3, 5);
+		InterfacePoint p = getPoint(3, 5);
 		assertTrue(p.equals(p));
-		InterfacePoint q = new ImplementPoint(3, 5);
+		InterfacePoint q = getPoint(3, 5);
 		assertTrue(p.equals(q) && q.equals(p));
-		assertTrue(!p.equals(new ImplementPoint(5, 3)));
-		assertTrue(!p.equals(new ImplementPoint(3, 6)));
-		assertTrue(!p.equals(new ImplementPoint(4, 5)));
+		assertTrue(!p.equals(getPoint(5, 3)));
+		assertTrue(!p.equals(getPoint(3, 6)));
+		assertTrue(!p.equals(getPoint(4, 5)));
 	}
 
 	@Test
 	public void testAdd()
 	{
-		InterfacePoint p = new ImplementPoint(3, 5), q = new ImplementPoint(2, 3);
-		assertTrue(p.add(p).equals(new ImplementPoint(6, 10)));
-		assertTrue(p.add(p).equals(new ImplementPoint(6, 10)));
-		assertTrue(p.add(q).equals(new ImplementPoint(5, 8)));
+		InterfacePoint p = getPoint(3, 5), q = getPoint(2, 3);
+		assertTrue(p.add(p).equals(getPoint(6, 10)));
+		assertTrue(p.add(p).equals(getPoint(6, 10)));
+		assertTrue(p.add(q).equals(getPoint(5, 8)));
 		assertTrue(p.add(q).equals(q.add(p)));
 	}
 
